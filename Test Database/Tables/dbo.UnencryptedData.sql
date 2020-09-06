e02f8a16da7a80e0b8a8b4e8b910507f7c76baab
Kevin Martin <kevin@themartins.com>
2020-09-06 11:30:14 -0500

    added test data for unencrypted data check

---
+++ Test Database/Tables/dbo.UnencryptedData.sql
@@ -0,0 +1,26 @@
+CREATE TABLE [dbo].[UnencryptedData]
+(
+[UnencryptedDataId] [int] NOT NULL,
+[Password] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[PasswordIsEncrypted] [nvarchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[LastPasswordChangedDate] [date] NULL,
+[PasswordConfigurationSettings] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[PasswordComplexityPattern] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[PasswordExpirationDate] [date] NULL,
+[IsPasswordChangedFlag] [bit] NULL,
+[LastPasswordFailedDate] [date] NULL,
+[MaxPasswordCharacters] [int] NULL,
+[MinPasswordCharacters] [int] NULL,
+[CreditCardNumber] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[CCN] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[CreditCardToken] [nvarchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[SSN] [char] (9) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[SocialSecurityNumber] [nchar] (11) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[PassportNumber] [int] NULL,
+[DLL] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[DriverLicenseNumber] [nvarchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[LicenseCount] [bigint] NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[UnencryptedData] ADD CONSTRAINT [PK_UnencryptedData] PRIMARY KEY CLUSTERED  ([UnencryptedDataId]) ON [PRIMARY]
+GO
